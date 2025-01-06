fdd5397943e9b1c336a1eb300d29ff42aace84a6
Taras Kalapun <t.kalapun@gmail.com>
2025-01-06 11:34:10 -0800

    Adding Newsletter section

--- run.sh
+++ run.sh
@@ -5,7 +5,10 @@
 # docker run -it --rm -v="${PWD}:/root/project:delegated" -p "35729:35729" --name jekyll -ti mrseccubus/github-pages
 # bundle exec jekyll serve -H 0.0.0.0 -P 4000 --livereload --incremental
 
-docker run -it --rm -v "${PWD}:/src/site" -p "4000:4000" -p "35729:35729" --name jekyll gh-pages jekyll serve -H 0.0.0.0 -P 4000 --livereload --incremental
+# docker run -it --rm -v "${PWD}:/src/site" -p "4000:4000" -p "35729:35729" --name jekyll gh-pages jekyll serve -H 0.0.0.0 -P 4000 --livereload --incremental
 
+#-c "bundle install && bundle exec
+#--watch --force_polling
+docker run --rm -it -p 4000:4000 -v ${PWD}:/src/site naoigcat/github-pages jekyll serve --H 0.0.0.0 --livereload --incremental
 
 
